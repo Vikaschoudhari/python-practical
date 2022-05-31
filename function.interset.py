04ca5388399ed87fda024848e0bfdab655bd0895
Vikaschoudhari <92860043+Vikaschoudhari@users.noreply.github.com>
2022-05-31 11:46:30 +0530

    Delete function.interset.py

--- function.interset.py
+++
@@ -1,9 +0,0 @@
-def simpleinterest(p,r,t):
-    SI=(p*r*t)/100
-    return SI
-p=int(input("enter the principal"))
-r=int(input("enter the rate"))
-t=int(input("enter the time"))
-
-SI=simpleinterest(p,r,t)
-print(SI)
